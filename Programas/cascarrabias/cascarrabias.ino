da07d80e306dfc667ca72eb172f551a3173a6e46
evafc003 <e.fernandezde.2021@alumnos.urjc.es>
2024-11-28 20:04:59 +0100

    Actualizado código

--- Programas/cascarrabias/cascarrabias.ino
+++ Programas/cascarrabias/cascarrabias.ino
@@ -27,7 +27,8 @@ int ARM_MED = 90;
 DHT dht(HUM, DHT11);
 
 // For control states
-bool pressed_button = false;
+bool first_time = false;
+bool secuence_done = false;
 unsigned long initial_time = 0;
 
 // FUNCTIONS WE NEED
@@ -115,55 +116,92 @@ void setup() {
 
 // Principal program
 void loop() {
-  get_in_box() // puede que esto no haga falta
+  get_in_box(); // puede que esto no haga falta
 
   if (digitalRead(SWITCH) == false) {
-    if (!pressed_button) {
-      // just pressed the button
-      pressed_button = true;
-      initial_time = millis();
+    // Elapsed time since button is pressed
+    unsigned long current_time = millis();
+    unsigned long elapsed_time = current_time - initial_time;
+    Serial.println(elapsed_time);
+    if (first_time == false){
+      for(int i=900; i>=800; i--) {
+        tone(BUZZER, i);
+        delay(10);
+      }
+      noTone(BUZZER);
+      get_out_of_box();
+      first_time = true;
     }
 
-    // Elapsed time since button is pressed
-    unsigned long elapsed_time = millis() - initial_time;
-    
-    if (elapsed_time <= 3000){ // SIENDO AMABLES
-      tone(BUZZER, 900);
-      delay(100);
-      tone(BUZZER, 800);
-      delay(100);
-      noTone(BUZZER);
-      delay(100);
+    if (elapsed_time > 10000 && elapsed_time <= 11000){ // SIENDO AMABLES
+      if (secuence_done == false){
+        tone(BUZZER, 900);
+        delay(100);
+        tone(BUZZER, 800);
+        delay(100);
+        noTone(BUZZER);
+        delay(100);
+        move_arm(1000);
+        secuence_done = true;
+      }
+      
+    }else if (elapsed_time > 20000 && elapsed_time <= 21000) { // SIENDO MENOS AMABLES
+      if (secuence_done == true){
+        tone(BUZZER, 900);
+        delay(1000);
+        tone(BUZZER, 800);
+        delay(1000);
+        noTone(BUZZER);
+        
+        delay(100);
+        move_arm(700);
+        secuence_done = false;
+      }
 
-      get_out_of_box();
-      delay(100);
-      move_arm(1000);
+    }else if (elapsed_time > 32000 && elapsed_time <= 33000) { // RECORDANDO NUESTRA EXISTENCIA 
+      if (secuence_done == false){
+        Serial.println("HOLA");
+        secuence_done = true;
+      }
 
-    }else if (elapsed_time > 11000 && elapsed_time <= 15000) { // SIENDO MENOS AMABLES
-      tone(BUZZER, 900);
-      delay(1000);
-      tone(BUZZER, 800);
-      delay(1000);
-      noTone(BUZZER);
-      
-      delay(100);
-      move_arm(700);
-    
-    }else if (elapsed_time > 15000 && elapsed_time <= 20000) { // RECORDANDO NUESTRA EXISTENCIA
-      
-    }else if (elapsed_time > 25000 && elapsed_time <= 30000) { // COMENZANDO A ENFADARNOS
-      
-    }else if (elapsed_time > 35000 && elapsed_time <= 40000) { // PERDIENDO LA CORDURA
-      
-    }else if (elapsed_time > 500000) { // VOLVIENDONOS LOCOS
-      
+    }else if (elapsed_time > 40000 && elapsed_time <= 41000) { // COMENZANDO A ENFADARNOS
+      if (secuence_done == true){
+        Serial.println("BUENAS");
+        secuence_done = false;
+      }
+
+    }else if (elapsed_time > 55000 && elapsed_time <= 60000) { // PERDIENDO LA CORDURA
+      if (secuence_done == false){
+        Serial.println("QUE TAL");
+        secuence_done = true;
+      }
+
+    }else if (elapsed_time > 800000) { // VOLVIENDONOS LOCOS
+      initial_time = 0;
+      Serial.println("BIEEEEEEEEEN");
     }
 
   } else {
-    // Button don't pressed, be inside the box and do nothing
-    if (pressed_button) {
-      pressed_button = false;
+    // Button don't pressed, be inside the box and sleep
+      if (first_time == true){
+        tone(BUZZER, 1000);
+        delay(100);
+        tone(BUZZER, 1200);
+        delay(100);
+        noTone(BUZZER);
+        first_time= false;
+      }
+      
       get_in_box();
-    }
+      unsigned long current_time = millis();
+
+      if (current_time - initial_time >= 10000) {
+        initial_time = current_time;
+        tone(BUZZER, 200);
+        delay(100);
+        tone(BUZZER, 250);
+        delay(1000);
+        noTone(BUZZER);
+      }
   } 
 }
